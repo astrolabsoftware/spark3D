1a4886e033187aec7277b51a0f8593dcba7f9dba
Julien <peloton@lal.in2p3.fr>
2018-05-14 21:53:47 +0200

    Remove unused methods in Utils

--- src/main/scala/com/spark3d/utils/Utils.scala
+++ src/main/scala/com/spark3d/utils/Utils.scala
@@ -13,31 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.spark3d
+package com.spark3d.Utils
 
-object Utils {
-
-  /**
-    * Convert declination into theta
-    *
-    * @param dec : (Double)
-    *   declination coordinate in degree
-    * @return theta coordinate in radian
-    *
-    */
-  def dec2theta(dec : Double) : Double = {
-    Math.PI / 2.0 - Math.PI / 180.0 * dec
-  }
-
-  /**
-    * Convert right ascension into phi
-    *
-    * @param ra : (Double)
-    *   RA coordinate in degree
-    * @return phi coordinate in radian
-    *
-    */
-  def ra2phi(ra : Double) : Double = {
-    Math.PI / 180.0 * ra
-  }
-}
+object Utils

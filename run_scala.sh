2ae2740709a92e9f1535fb1c17047003d09aadc7
Julien <peloton@lal.in2p3.fr>
2018-05-22 15:22:55 +0200

    Add missing file + main class for example

---
+++ run_scala.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright 2018 Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## SBT Version
+SBT_VERSION=2.11.8
+SBT_VERSION_SPARK=2.11
+
+## Package version
+VERSION=0.1.0
+
+# Package it
+sbt ++${SBT_VERSION} package
+
+# Parameters (put your file)
+fitsfn="file://$PWD/src/test/resources/astro_obs.fits"
+hdu=1
+columns="RA,Dec,Z_COSMO"
+
+# Run it!
+spark-submit \
+  --master local[*] \
+  --class com.spark3d.examples.OnionSpace \
+  --jars lib/jhealpix.jar \
+  --packages com.github.JulienPeloton:spark-fits_2.11:0.3.0 \
+  target/scala-${SBT_VERSION_SPARK}/spark3d_${SBT_VERSION_SPARK}-${VERSION}.jar \
+  $fitsfn $hdu $columns

ef0aa33cce6585ffc7336720f9b5f34537fd534b
Mayur Bhosale <mayurdb31@gmail.com>
2018-06-25 12:30:41 +0530

    added KNN naive implementation

--- src/main/scala/com/spark3d/utils/GeometryObjectComparator.scala
+++ src/main/scala/com/spark3d/utils/GeometryObjectComparator.scala
@@ -1,16 +1,49 @@
+/*
+ * Copyright 2018 Mayur Bhosale
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.spark3d.utils
 
 import com.spark3d.geometryObjects.Point3D
 import com.spark3d.geometryObjects.Shape3D.Shape3D
 
-class GeometryObjectComparator[A <: Shape3D]
-    (val queryObjectCenter: Point3D) extends Ordering[A] {
+//class GeometryObjectComparator(val queryObjectCenter: Point3D) extends Ordering[Shape3D] {
+//
+//  override def compare(x: Shape3D, y: Shape3D): Int = {
+//    val dist1 = x.center.distanceTo(queryObjectCenter)
+//    val dist2 = y.center.distanceTo(queryObjectCenter)
+//
+//    if (dist1 <= dist2) {
+//      return -1
+//    } else {
+//      return 1
+//    }
+//  }
+//}
+
+
+class GeometryObjectComparator[A <: Shape3D](val queryObjectCenter: Point3D) extends Ordering[A] {
 
   override def compare(x: A, y: A): Int = {
     val dist1 = x.center.distanceTo(queryObjectCenter)
     val dist2 = y.center.distanceTo(queryObjectCenter)
 
-    dist1 compare dist2
+    if (dist1 <= dist2) {
+      return -1
+    } else {
+      return 1
+    }
   }
-
 }
---
+++ src/test/scala/com/spark3d/spatialOperator/SpatialQueryTest.scala
@@ -0,0 +1,5 @@
+package com.spark3d.spatialOperator
+
+class SpatialQueryTest {
+
+}

6e31ff1a7c63ecc4f9095708c2f613c8e219438b
Julien <peloton@lal.in2p3.fr>
2018-07-26 12:52:22 +0200

    Change level of persistence to MEMORY_ONLY in the Partitioning example

--- src/main/scala/com/spark3d/examples/Partitioning.scala
+++ src/main/scala/com/spark3d/examples/Partitioning.scala
@@ -21,6 +21,7 @@ import com.astrolabsoftware.spark3d.spatial3DRDD._
 import com.astrolabsoftware.spark3d.utils.GridType
 
 // Spark lib
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
@@ -64,7 +65,9 @@ object Partitioning {
 
     // Load the data
     val options = Map("hdu" -> hdu)
-    val pRDD = new Point3DRDD(spark, fn_fits, columns, isSpherical, "fits", options)
+    val pRDD = new Point3DRDD(
+      spark, fn_fits, columns, isSpherical, "fits", options, StorageLevel.MEMORY_ONLY
+    )
 
     // Partition it
     val rdd = mode match {

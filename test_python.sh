e21b05e06ef7d4951424d2768e9275951b418a27
Julien <peloton@lal.in2p3.fr>
2018-08-03 23:08:40 +0200

    Avoid python version conflict

--- test_python.sh
+++ test_python.sh
@@ -3,7 +3,6 @@
 ## Script to measure the coverage of the test suite (via doctest).
 ## Launch it using ./coverage
 ## and open the html files under the folder htmlcov/
-## Skip xpure.py as it is not really part of the pipeline
 cd pyspark3d
 for i in *.py
 do

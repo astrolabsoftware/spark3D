e8753dac8128e88b96b366b20eec90054d14fc27
Julien <peloton@lal.in2p3.fr>
2018-11-16 09:58:53 +0100

    Split repartitioning in tw: Adding a partitioner AND trigger the partitioning

--- src/main/scala/com/spark3d/Repartitioning.scala
+++ src/main/scala/com/spark3d/Repartitioning.scala
@@ -17,92 +17,200 @@ package com.astrolabsoftware.spark3d
 
 import java.util.HashMap
 
-import org.apache.spark.Partitioner
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.udf
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.spark_partition_id
 
 import com.astrolabsoftware.spark3d.Partitioners
+import com.astrolabsoftware.spark3d.spatialPartitioning.KeyPartitioner
 
 /**
-  * Main object containing methods to repartition DataFrames
+  * Main object containing methods to repartition DataFrames.
+  * Unlike RDDs, Apache Spark Dataset & DataFrame cannot use custom partitioner.
+  * This work is an attempt to bridge the gap and allow custom repartitioning of DataFrame.
   *
-  * partitionBy(DataFrame, SpatialPartitioner, Map[String, String]): DataFrame
+  * Note that there exists some methods to repartition DataFrame, but they are not
+  * deterministic (i.e. they use sampling). See SPARK-26024 for example.
   */
 object Repartitioning {
 
   /**
-    * Method to repartition a DataFrame. This method allows to use a custom
-    * partitioner (SpatialPartitioner). {{options}} must contain four entries:
-    *   - gridtype: the type of repartitioning. Available: onion, octree.
+    * Add a DataFrame column describing the partitioning. This method allows to use a custom
+    * partitioner (SpatialPartitioner). Note that no data movement (shuffle) is performed yet here,
+    * as we just describe how the repartitioning should be done. Use `partitionBy` to
+    * trigger it.
+    *
+    *`options` must contain four entries:
+    *   - gridtype: the type of repartitioning. Available: current (no repartitioning), onion, octree.
     *   - geometry: geometry of objects: points, spheres, or boxes
     *   - coordSys: coordinate system: spherical or cartesian
     *   - colnames: comma-separated names of the spatial coordinates. For points,
-    *   must be "x,y,z" or "r,theta,phi". For spheres, must be "x,y,z,R" or
-    *   "r,theta,phi,R".
+    *     must be "x,y,z" or "r,theta,phi". For spheres, must be "x,y,z,R" or
+    *     "r,theta,phi,R".
     *
     * @param df : Input DataFrame
     * @param options : Map[String, String] containing metadata (see above).
-    * @return repartitioned DataFrame. Note that an additional column {{partition_id}} is added.
+    * @param numPartitions : (optional) The number of partitions wanted. -1 by default,
+    *   i.e. the code will try to guess something.
+    * @return repartitioned DataFrame. Note that an additional column `partition_id` is added.
     */
-  def partitionBy(df : DataFrame, options: Map[String, String], numPartitions : Int = -1) : DataFrame = {
+  def addSPartitioning(df : DataFrame, options: Map[String, String], numPartitions : Int = -1) : DataFrame = {
 
-    // Geometry of objects
-    val geometry : String = options("geometry")
-
-    // Definition of the coordinate system. Spherical or cartesian
-    val isSpherical : Boolean = options("coordSys") match {
-      case "spherical" => true
-      case _ => false
+    // Change the number of partitions if wanted
+    val numOfPartitions = numPartitions match {
+      case -1 => df.rdd.getNumPartitions
+      case x if x > 0 => numPartitions
+      case _ => throw new AssertionError(s"""
+        The number of partitions must be strictly greater than zero!
+        Otherwise leave it unset to take the number of partitions
+        of the input DataFrame.
+        (You put: $numPartitions)
+        """)
     }
 
-    // Column names must be comma-separated.
-    val colnames : Array[String] = options("colnames").split(",")
-
-    val P = new Partitioners(df, options)
-    val partitioner = P.get(numPartitions)
-
-    // Add a column with the new partition indices
-    val dfup = geometry match {
-      case "points" => {
-        // UDF for the repartitioning
-        val placePointsUDF = udf[Int, Double, Double, Double, Boolean](partitioner.placePoints)
-
-        df.withColumn("partition_id",
-          placePointsUDF(
-            col(colnames(0)).cast("double"),
-            col(colnames(1)).cast("double"),
-            col(colnames(2)).cast("double"),
-            lit(isSpherical)
-          )
-        )
+    // Branch out: keep current partitioning or apply a custom one.
+    val dfout = options("gridtype") match {
+      // Return current DF with default partition ID
+      case "current" => {
+        df.repartition(numOfPartitions).withColumn("partition_id", spark_partition_id())
       }
-      case "spheres" => {
-        // UDF for the repartitioning
-        val placePointsUDF = udf[Int, Double, Double, Double, Double, Boolean](partitioner.placeSpheres)
 
-        df.withColumn("partition_id",
-          placePointsUDF(
-            col(colnames(0)).cast("double"),
-            col(colnames(1)).cast("double"),
-            col(colnames(2)).cast("double"),
-            col(colnames(3)).cast("double"),
-            lit(isSpherical)
-          )
-        )
+      // Other implemented repartitioners
+      case grid @ ("onion" | "octree") => {
+        // Geometry of objects
+        val geometry : String = options("geometry")
+
+        // Definition of the coordinate system. Spherical or cartesian
+        val isSpherical : Boolean = options("coordSys") match {
+          case "spherical" => true
+          case "cartesian" => false
+          case _ => throw new AssertionError("""
+            Coordinate system not understood! You must choose between:
+            spherical, cartesian
+            """)
+        }
+
+        // Column names must be comma-separated.
+        val colnames : Array[String] = options("colnames").split(",")
+
+        val P = new Partitioners(df, options)
+        val partitioner = P.get(numOfPartitions)
+
+        // Add a column with the new partition indices
+        val dfExt = geometry match {
+          case "points" => {
+            // UDF for the repartitioning
+            val placePointsUDF = udf[Int, Double, Double, Double, Boolean](partitioner.placePoints)
+
+            df.withColumn("partition_id",
+              placePointsUDF(
+                col(colnames(0)).cast("double"),
+                col(colnames(1)).cast("double"),
+                col(colnames(2)).cast("double"),
+                lit(isSpherical)
+              )
+            )
+          }
+          case "spheres" => {
+            // UDF for the repartitioning
+            val placePointsUDF = udf[Int, Double, Double, Double, Double, Boolean](partitioner.placeSpheres)
+
+            df.withColumn("partition_id",
+              placePointsUDF(
+                col(colnames(0)).cast("double"),
+                col(colnames(1)).cast("double"),
+                col(colnames(2)).cast("double"),
+                col(colnames(3)).cast("double"),
+                lit(isSpherical)
+              )
+            )
+          }
+          case _ => throw new AssertionError("""
+            Geometry not understood! You must choose between:
+            points, spheres, boxes
+            """)
+        }
+        dfExt
       }
-      // Need to handle errors!
+      case _ => throw new AssertionError("""
+        Gridtype not understood! You must choose between:
+        onion, octree, or current
+        """)
     }
+
+    dfout
+  }
+
+  /**
+    * Repartition a DataFrame according to a column containing explicit ordering.
+    * Note this is not re-ordering elements, but making new partitions with objects
+    * having the same partition ID defined by one of the DataFrame column (i.e. shuffling).
+    *
+    * @param df : input DataFrame.
+    * @param colname : Column name describing the repartitioning. Typically Ints.
+    * @param numPartitions : Optional. Number of partitions. If not provided the code will
+    *   guess the number of partitions by counting the number of distinct elements of
+    *   the repartitioning column. As it can be costly, you can provide manually this information.
+    *
+    * In other words, the column used for the partitioning should contain Ints describing
+    * the partition indices:
+    *
+    * > df.show()
+    *  +-------------------+-------------------+------------------+------------+
+    *  |            Z_COSMO|                 RA|               Dec|partition_id|
+    *  +-------------------+-------------------+------------------+------------+
+    *  |   0.54881352186203|    1.2320476770401| 2.320105791091919|           0|
+    *  | 0.7151893377304077|0.12929722666740417|1.3278003931045532|           1|
+    *  | 0.6027633547782898|  2.900634288787842| 2.996480941772461|           0|
+    *  | 0.5448831915855408| 1.2762248516082764|0.5166937112808228|           0|
+    *  |0.42365479469299316|  2.966549873352051|1.4932578802108765|           2|
+    *  +-------------------+-------------------+------------------+------------+
+    *
+    * will be repartitioned according to partition_id in 3 partitions (0, 1, 2) as
+    *
+    * > dfp = partitionBy(df, "partition_id")
+    * > dfp.show()
+    *  +-------------------+-------------------+------------------+------------+
+    *  |            Z_COSMO|                 RA|               Dec|partition_id|
+    *  +-------------------+-------------------+------------------+------------+
+    *  |   0.54881352186203|    1.2320476770401| 2.320105791091919|           0|
+    *  | 0.6027633547782898|  2.900634288787842| 2.996480941772461|           0|
+    *  | 0.5448831915855408| 1.2762248516082764|0.5166937112808228|           0|
+    *  | 0.7151893377304077|0.12929722666740417|1.3278003931045532|           1|
+    *  |0.42365479469299316|  2.966549873352051|1.4932578802108765|           2|
+    *  +-------------------+-------------------+------------------+------------+
+    */
+  def partitionBy(df: DataFrame, colname: String, numPartitions: Int = -1): DataFrame = {
+
+    // Compute the number of partitions if not provided
+    val numOfPartitions = numPartitions match {
+      case -1 => df.select(col(colname)).distinct().collect().size
+      case x if x > 0 => numPartitions
+      case _ => throw new AssertionError("""
+        The number of partitions must be strictly greater than zero!
+        Otherwise leave it unset to take the number of partitions
+        of the input DataFrame.
+        (You put: $numPartitions)
+        """)
+     }
+
+    // Where is the column of interest.
+    val position = df.columns.indexOf(colname)
+
+    // Simple key partitioner
+    val kp = new KeyPartitioner(numOfPartitions)
 
     // Apply the partitioning in the RDD world
-    val rdd = dfup.rdd
-      .map(x => (x(x.size - 1), x))
-      .partitionBy(partitioner)
+    val rdd = df.rdd
+      .map(x => (x(position), x))
+      .partitionBy(kp)
       .values
 
     // Go back to DF
-    SparkSession.getActiveSession.get.createDataFrame(rdd, dfup.schema)
+    SparkSession.getActiveSession.get.createDataFrame(rdd, df.schema)
+
   }
 }
--- src/main/scala/com/spark3d/package.scala
+++ src/main/scala/com/spark3d/package.scala
@@ -16,14 +16,7 @@
 package com.astrolabsoftware
 
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.Partitioner
-import org.apache.spark.sql.functions.udf
-import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.functions.lit
 
-import org.apache.spark.sql.SparkSession
-
-import com.astrolabsoftware.spark3d.spatialPartitioning.SpatialPartitioner
 import com.astrolabsoftware.spark3d.Repartitioning
 
 package object spark3d {
@@ -34,24 +27,84 @@ package object spark3d {
   implicit class DFExtended(df : DataFrame) {
 
     /**
-      * Add a new method {{partitionBy}} to DataFrame.
-      * This method allows to use a custom partitioner (SpatialPartitioner).
-      * {{options}} must contain four entries:
-      *   - gridtype: the type of repartitioning. Available: onion, octree.
+      * Add a DataFrame column describing the partitioning. This method allows to use a custom
+      * partitioner (SpatialPartitioner). Note that no data movement (shuffle) is performed here,
+      * as we just describe how the repartitioning should be done. Use `partitionBy` to
+      * trigger it.
+      *
+      *`options` must contain four entries:
+      *   - gridtype: the type of repartitioning. Available: current (no repartitioning), onion, octree.
       *   - geometry: geometry of objects: points, spheres, or boxes
       *   - coordSys: coordinate system: spherical or cartesian
       *   - colnames: comma-separated names of the spatial coordinates. For points,
-      *   must be "x,y,z" or "r,theta,phi". For spheres, must be "x,y,z,R" or
-      *   "r,theta,phi,R".
-      *
-      * See Repartitioning.partitionBy for the implementation.
+      *     must be "x,y,z" or "r,theta,phi". For spheres, must be "x,y,z,R" or
+      *     "r,theta,phi,R".
       *
       * @param options : Map[String, String] containing metadata (see above).
-      * @return (Int) The key of the partition as Int.
-      *
+      * @param numPartitions : (optional) The number of partitions wanted. -1 by default,
+      *   i.e. the code will try to guess something.
+      * @return repartitioned DataFrame. Note that an additional column `partition_id` is added.
       */
-    def partitionBy(options: Map[String, String], numPartitions : Int = -1) : DataFrame = {
-      Repartitioning.partitionBy(df, options, numPartitions)
+    def addSPartitioning(options: Map[String, String], numPartitions : Int = -1) : DataFrame = {
+      Repartitioning.addSPartitioning(df, options, numPartitions)
+    }
+
+    /**
+      * Repartition a DataFrame according to a column containing explicit ordering.
+      * Note this is not re-ordering elements, but making new partitions with objects
+      * having the same partition ID defined by one of the DataFrame column (i.e. shuffling).
+      *
+      * @param colname : Column name describing the repartitioning. Typically Ints.
+      * @param numPartitions : Optional. Number of partitions. If not provided the code will
+      *   guess the number of partitions by counting the number of distinct elements of
+      *   the repartitioning column. As it can be costly, you can provide manually this information.
+      *
+      * In other words, the column used for the partitioning should contain Ints describing
+      * the partition indices:
+      *
+      * > df.show()
+      *  +-------------------+-------------------+------------------+
+      *  |            Z_COSMO|                 RA|               Dec|
+      *  +-------------------+-------------------+------------------+
+      *  |   0.54881352186203|    1.2320476770401| 2.320105791091919|
+      *  | 0.7151893377304077|0.12929722666740417|1.3278003931045532|
+      *  | 0.6027633547782898|  2.900634288787842| 2.996480941772461|
+      *  | 0.5448831915855408| 1.2762248516082764|0.5166937112808228|
+      *  |0.42365479469299316|  2.966549873352051|1.4932578802108765|
+      *  +-------------------+-------------------+------------------+
+      *
+      * > options = Map(
+      *     "geometry" -> "points",
+      *     "colnames" -> "Z_COSMO,RA,DEC",
+      *     "coordSys" -> "spherical",
+      *     "gridtype" -> "LINEARONIONGRID")
+      * > val dfExt = df.addSPartitioning(options, 3)
+      * > dfExt.show()
+      *  +-------------------+-------------------+------------------+------------+
+      *  |            Z_COSMO|                 RA|               Dec|partition_id|
+      *  +-------------------+-------------------+------------------+------------+
+      *  |   0.54881352186203|    1.2320476770401| 2.320105791091919|           0|
+      *  | 0.7151893377304077|0.12929722666740417|1.3278003931045532|           1|
+      *  | 0.6027633547782898|  2.900634288787842| 2.996480941772461|           0|
+      *  | 0.5448831915855408| 1.2762248516082764|0.5166937112808228|           0|
+      *  |0.42365479469299316|  2.966549873352051|1.4932578802108765|           2|
+      *  +-------------------+-------------------+------------------+------------+
+      *
+      * will be repartitioned according to partition_id in 3 partitions (0, 1, 2) as
+      * > val dfp = dfExt.partitionBy("partition_id")
+      * > dfp.show()
+      *  +-------------------+-------------------+------------------+------------+
+      *  |            Z_COSMO|                 RA|               Dec|partition_id|
+      *  +-------------------+-------------------+------------------+------------+
+      *  |   0.54881352186203|    1.2320476770401| 2.320105791091919|           0|
+      *  | 0.6027633547782898|  2.900634288787842| 2.996480941772461|           0|
+      *  | 0.5448831915855408| 1.2762248516082764|0.5166937112808228|           0|
+      *  | 0.7151893377304077|0.12929722666740417|1.3278003931045532|           1|
+      *  |0.42365479469299316|  2.966549873352051|1.4932578802108765|           2|
+      *  +-------------------+-------------------+------------------+------------+
+      */
+    def partitionBy(colname: String, numPartitions: Int = -1): DataFrame = {
+      Repartitioning.partitionBy(df, colname, numPartitions)
     }
   }
 }

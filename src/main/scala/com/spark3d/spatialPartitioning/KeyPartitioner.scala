6f6427cf4dd3de858e5e2c3039e857e6d9f55dfe
Julien <peloton@lal.in2p3.fr>
2018-11-16 10:00:47 +0100

    Add simple KeyPartitioner to trigger custom partitioning

---
+++ src/main/scala/com/spark3d/spatialPartitioning/KeyPartitioner.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 Julien Peloton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.astrolabsoftware.spark3d.spatialPartitioning
+
+// Spark built-in partitioner
+import org.apache.spark.Partitioner
+
+/**
+  * Simple Key partitioner (maybe it exists already in Apache Spark?).
+  * Produces as many partitions as different keys.
+  *
+  */
+class KeyPartitioner(numOfPartitions: Int) extends Partitioner with Serializable {
+
+  /**
+    * Return the total number of partitions in a RDD
+    *
+    * @return (Int) the number of partitions
+    */
+  override def numPartitions : Int = {
+    numOfPartitions
+  }
+
+  /**
+    * Method to return the index of a partition
+    *
+    * @param key : the key of the partition (from key/value)
+    * @return (Int) the key of the partition as Int.
+    */
+  override def getPartition(key : Any) : Int = {
+    key.asInstanceOf[Int]
+  }
+}

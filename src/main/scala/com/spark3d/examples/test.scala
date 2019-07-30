6d76411d8492914a8351c859838d059738ac3146
alia <alia@vm-75222.lal.in2p3.fr>
2019-07-30 14:04:49 +0200

    Validation is addet

--- src/main/scala/com/spark3d/examples/test.scala
+++ src/main/scala/com/spark3d/examples/test.scala
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 package com.astrolabsoftware.spark3d.examples
+import scala.math._
 
 // spark3D implicits
 import com.astrolabsoftware.spark3d._
@@ -29,7 +30,7 @@ import com.astrolabsoftware.spark3d.geometryObjects.BoxEnvelope
 // Spark lib
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
+//import org.apache.spark.sql.functions._
 
 // Logger
 import org.apache.log4j.Level
@@ -58,14 +59,14 @@ object Test {
    
     // Data file
     val fn_fits = args(0).toString
-    println("arg0: "+ fn_fits)
+    
     // HDU
     val hdu = args(1).toString
-     println("arg1: "+ hdu)
+     
 
     // Column names
     val columns = args(2).toString
-    println("arg2: "+columns)
+    
   
   // isSpherical
   val isSpherical : String = args(3).toBoolean match {
@@ -74,23 +75,22 @@ object Test {
 }
     // partitioning
     val grid = args(4).toString
-     println("arg4 "+grid)
+     
     // partitions
     val part = args(5).toInt
-    println("arg 5: "+ part)
+     
     // Mode
     val mode = args(6).toString
-    println("Arg6: "+mode)
+     
     // Load the data
     ///////////////////////////////spark.sparkContext
     //val  df = spark.read.format("csv").option("header", "true").load("hdfs://134.158.75.222:8020/user/alia/example3D.csv")
     //val  df = spark.read.format("csv").option("header", "true").load("hdfs://134.158.75.222:8020/user/alia/test2d.csv")
-    val df = spark.read.format("fits").option("hdu", 1).load(fn_fits)
+     val df = spark.read.format("fits").option("hdu", 1).load(fn_fits)
      
     
     //////////////////////////////
      
-      println( df.count())
    val options = Map(
     "geometry" -> "points",
     "colnames" -> columns,
@@ -98,15 +98,35 @@ object Test {
     "gridtype" -> grid)
 
 
-    // val mypartitioner = new Partitioners(df, options)
-  // val myrawRDD = mypartitioner.rawRDD
+     
 
-  // println(myrawRDD.take(1))
+  /**
+   * Validation
+   */
+  /**
+   * Find the number of levels
+   */
+  val log2 = (x: Int) => log10(x)/log10(2.0)
 
+  val maximumLevel=(log2(df.count.asInstanceOf[Int]+1))-2 
+  //Find the number of partitions
+  val maximumPartitions=pow(maximumLevel,2)
+  
+  if(log2(part)%1==0)
+     if(part<=maximumPartitions){
+         val df_colid = df.prePartition(options, part)
+         val number1=df_colid.repartitionByCol("partition_id", true, part).mapPartitions(part => Iterator(part.size)).collect().toList
+         println("number1: "+ number1)
+      }
+      else 
+         println("Please, Maximum number of partitions is "+ maximumPartitions)
+   else 
+    println("Please determine the number of partitions as 1, 2, 4, 8 and so on")
+        
 
+ 
+  //  val df_colid = df.prePartition(options, part)
 
-//partition_id
-    val df_colid = df.prePartition(options, part)
    // df_colid.show(100,false)
    // df_colid.groupBy("partition_id").count().show(50,false)   
     // df_colid.printSchema()
@@ -117,19 +137,10 @@ object Test {
   //    val number= df_colid.repartitionByCol("partition_id", true, part)
           
 
-   val number1=df_colid.repartitionByCol("partition_id", true, part).mapPartitions(part => Iterator(part.size)).collect().toList
-    println("number1: "+ number1)
+  // val number1=df_colid.repartitionByCol("partition_id", true, part).mapPartitions(part => Iterator(part.size)).collect().toList
+  //  println("number1: "+ number1)
    
-     // val kdtree=new KDtree( )
-     // kdtree.printKDtree()
-    // val point=new Point3D(3,1,4,false)
-    // val point1=new Point3D(2,3,7,false)
-    // val point2=new Point3D(4,3,4,false)
-   
-    // kdtree.insert(point)
-    // kdtree.insert(point1)
-    // kdtree.insert(point2)
-    // kdtree.printKDtree(kdtree)
+     
 
   }
 }
